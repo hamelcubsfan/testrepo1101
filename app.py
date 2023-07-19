b7b62bb474aad96b973821d8eb8b42efd858e52c
Marc Hamel <hamelcubsfan@gmail.com>
2023-07-19 13:34:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,83 @@
 import streamlit as st
-from langchain.chains import StuffDocumentsChain, LLMChain
-from langchain.prompts import PromptTemplate
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.chains import StuffDocumentsChain
 from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import PromptTemplate
 
-# Define the prompt templates
-document_prompt_template = PromptTemplate(
-    input_variables=["page_content"],
-    template="{page_content}"
-)
+# Create Streamlit interface
+st.title("Personalized Outreach Generator")
 
-map_prompt_template = PromptTemplate.from_template(
-    "Map this content: {context}"
-)
+api_key = st.text_input("Enter your OpenAI API Key")
+source_url = st.text_input("Enter the source URL (LinkedIn profile, tweets, or blog posts)")
 
-combine_prompt_template = PromptTemplate.from_template(
-    "Combine this content: {context}"
-)
+map_prompt = """Below is a section of a website about {prospect}
 
-# Initialize the language model
-llm = OpenAI(temperature=0.5)
+Write a concise summary about {prospect}. If the information is not about {prospect}, exclude it from your summary.
 
-# Create two LLM chains: one for mapping, one for combining
-map_llm_chain = LLMChain(
-    llm=llm,
-    prompt=map_prompt_template
-)
+{text}
 
-combine_llm_chain = LLMChain(
-    llm=llm,
-    prompt=combine_prompt_template
-)
+% CONCISE SUMMARY:"""
 
-# The name of the variable in which to put the documents
-document_variable_name = "context"
+combine_prompt = """
+Your goal is to write a personalized outbound email from {sales_rep}, a sales rep at {company} to {prospect}.
 
-# Create a StuffDocumentsChain instance
-summarize_chain = StuffDocumentsChain(
-    llm_chain=map_llm_chain,  # or combine_llm_chain depending on your use case
-    document_prompt=document_prompt_template,
-    document_variable_name=document_variable_name
-)
+A good email is personalized and combines information about the two companies on how they can help each other.
+Be sure to use value selling: A sales methodology that focuses on how your product or service will provide value to the customer instead of focusing on price or solution.
 
-# Streamlit app
-st.title('Summarize Documents')
+% INFORMATION ABOUT {company}:
+{company_information}
 
-# Get the input from the user
-input_docs = st.text_area("Input Documents", value='', height=None, max_chars=None, key=None)
+% INFORMATION ABOUT {prospect}:
+{text}
 
-# If there is any input, process it
-if input_docs:
-    # Split the input into separate documents
-    input_docs_list = input_docs.split('\n')
+% INCLUDE THE FOLLOWING PIECES IN YOUR RESPONSE:
+- Start the email with the sentence: "We love that {prospect} helps teams..." then insert what they help teams do.
+- The sentence: "We can help you do XYZ by ABC" Replace XYZ with what {prospect} does and ABC with what {company} does 
+- A 1-2 sentence description about {company}, be brief
+- End your email with a call-to-action such as asking them to set up time to talk more
 
-    # Use the StuffDocumentsChain to summarize the documents
-    result = summarize_chain({"input_documents": input_docs_list})
+% YOUR RESPONSE:
+"""
 
-    # Display the result
-    st.text_area("Result", value=result, height=None, max_chars=None, key=None)
+company_information = """
+* RapidRoad helps product teams build product faster
+* We have a platform that allows product teams to talk more, exchange ideas, and listen to more customers
+* Automated project tracking: RapidRoad could use machine learning algorithms to automatically track project progress, identify potential bottlenecks, and suggest ways to optimize workflows. This could help product teams stay on track and deliver faster results.
+* Collaboration tools: RapidRoad could offer built-in collaboration tools, such as shared task lists, real-time messaging, and team calendars. This would make it easier for teams to communicate and work together, even if they are in different locations or time zones.
+* Agile methodology support: RapidRoad could be specifically designed to support agile development methodologies, such as Scrum or Kanban. This could include features like sprint planning, backlog management, and burndown charts, which would help teams stay organized and focused on their goals.
+"""
+
+if st.button("Generate"):
+    if api_key and source_url:
+        # Initialize the necessary classes
+        lang_model = OpenAI(temperature=0.5)
+        url_loader = UnstructuredURLLoader(urls=[source_url])
+        text_splitter = RecursiveCharacterTextSplitter()
+        map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text", "prospect"])
+        combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=["sales_rep", "company", "prospect", "text", "company_information"])
+        map_llm_chain = LLMChain(llm=lang_model, prompt=map_prompt_template)
+        combine_llm_chain = LLMChain(llm=lang_model, prompt=combine_prompt_template)
+        document_variable_name = "context" 
+        summarize_chain = StuffDocumentsChain(
+            llm_chain=map_llm_chain,  # or combine_llm_chain depending on your use case
+            document_prompt=map_prompt_template,
+            document_variable_name=document_variable_name
+        )
+
+        # Load and prepare the data
+        documents = url_loader.load()
+
+        # Extract the page_content from each Document object
+        texts = [doc.page_content for doc in documents]
+
+        # Split the texts into chunks
+        documents = text_splitter.create_documents(texts)
+
+        # Generate the personalized outreach email
+        email = summarize_chain.run({"input_documents": documents, "company": "Your Company", "company_information": company_information, "sales_rep": "Your Name", "prospect": "Prospect Name"})
+        
+        st.write(email)
+    else:
+        st.write("Please provide both the API key and source URL.")
