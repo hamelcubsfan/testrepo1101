03ff0292fb3206b2c69d662a3cb6ab90310f48af
Marc Hamel <hamelcubsfan@gmail.com>
2023-07-19 16:49:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,39 @@
 import streamlit as st
-from langchain import PromptTemplate
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+from langchain import DocumentLoader
+from langchain.load import load_chain
+from langchain.llms.openai import ChatOpenAI
+from langchain.llms.openai import OpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.summarize import load_summarize_chain
-from langchain.prompts import PromptTemplate
 import requests
 from bs4 import BeautifulSoup
 from markdownify import markdownify as md
 
-def main():
-    st.title("LinkedIn Profile Summarizer")
+# Setup the langchain library
+chain = load_chain("map_reduce_llm")
+llm = ChatOpenAI(temperature=.25, model_name='gpt-3.5-turbo-16k')
 
-    # Streamlit text input for LinkedIn profile URL
-    linkedin_url = st.text_input("Please input the LinkedIn profile URL you want to summarize:")
-    
-    # Streamlit text input for OpenAI API key
-    OPENAI_API_KEY = st.text_input("Please input your OpenAI API key:")
+# Get the LinkedIn url from the user
+linkedin_url = st.text_input("Enter the LinkedIn URL:")
 
-    # Streamlit button to execute
-    execute_button = st.button("Execute")
+# Get the OpenAI API Key from the user
+OPENAI_API_KEY = st.text_input("Enter the OpenAI API Key:", type="password")
 
-    # Function to scrape a website
-    def pull_from_website(url):
-        try:
-            response = requests.get(url)
-        except:
-            print ("Whoops, error")
-            return
-
+if st.button('Generate'):
+    if linkedin_url and OPENAI_API_KEY:
+        llm.api_key = OPENAI_API_KEY
+        
+        # Fetch and parse the LinkedIn page
+        response = requests.get(linkedin_url)
         soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Extract only the section containing the profile
-        profile_section = soup.find('section', {'id': 'profile'})
-
-        if profile_section is None:
-            return None
-
-        text = profile_section.get_text()
+        text = soup.get_text()
         text = md(text)
-
-        return text
-
-    if execute_button:
-        if linkedin_url:
-            st.write(f"Fetching data from {linkedin_url}")
-            fetched_data = pull_from_website(linkedin_url)
-            
-            if fetched_data is None:
-                st.write("Failed to fetch data from the website.")
-            else:
-                st.write("Fetched data successfully. Now splitting into documents.")
-                # Split the fetched data into documents
-                text_splitter = RecursiveCharacterTextSplitter(chunk_size=20000, chunk_overlap=2000)
-                docs = text_splitter.create_documents([fetched_data])
-
-                if len(docs) == 0:
-                    st.write("Failed to split the fetched data into documents.")
-                else:
-                    st.write("Data split into documents successfully. Now generating summary.")
-                    # Generate the summary
-                    llm = ChatOpenAI(temperature=.25, model_name='gpt-3.5-turbo-16k', api_key=OPENAI_API_KEY)
-                    chain = load_summarize_chain(llm)
-
-                    output = chain({"input_documents": docs})
-
-                    st.write("Generated summary successfully.")
-                    st.write(output['output_text'])
-
-if __name__ == "__main__":
-    main()
+        
+        # Split the text into chunks
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=20000, chunk_overlap=2000)
+        docs = text_splitter.create_documents([text])
+        
+        # Use the langchain library to generate the output
+        output = chain({"input_documents": docs})
+        st.write(output)
+    else:
+        st.write("Please enter both the LinkedIn URL and the OpenAI API Key.")
