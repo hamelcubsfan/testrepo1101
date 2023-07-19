37d8a06c71433f380681865a3ac188f1ffde717b
Marc Hamel <hamelcubsfan@gmail.com>
2023-07-19 16:42:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,82 +1,69 @@
-import os
-import requests
 import streamlit as st
-from bs4 import BeautifulSoup
-from markdownify import markdownify as md
 from langchain import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
+from langchain.prompts import PromptTemplate
+import requests
+from bs4 import BeautifulSoup
+from markdownify import markdownify as md
+from langchain.document_loaders import Document
 
-# Scrapping function
-def pull_from_website(url):
-    try:
-        response = requests.get(url)
-    except:
-        print ("Whoops, error")
-        return
-    
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = soup.get_text()
-    text = md(text)
-     
-    return text
-
-# Streamlit main function
 def main():
     st.title("LinkedIn Profile Summarizer")
-    url = st.text_input("Enter LinkedIn profile URL")
-    OPENAI_API_KEY = st.text_input("Enter OpenAI API Key", type="password")
-    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-    if st.button("Generate Summary"):
-        website_data = pull_from_website(url)
-        
-        if not website_data:
-            st.error("Failed to fetch data from the website. Please check the URL.")
+    # Streamlit text input for LinkedIn profile URL
+    linkedin_url = st.text_input("Please input the LinkedIn profile URL you want to summarize:")
+    
+    # Streamlit text input for OpenAI API key
+    OPENAI_API_KEY = st.text_input("Please input your OpenAI API key:")
+
+    # Function to scrape a website
+    def pull_from_website(url):
+        try:
+            response = requests.get(url)
+        except:
+            print ("Whoops, error")
             return
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=20000, chunk_overlap=2000)
-        docs = text_splitter.create_documents([website_data])
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract only the section containing the profile
+        profile_section = soup.find('section', {'id': 'profile'})
+
+        if profile_section is None:
+            return None
+
+        text = profile_section.get_text()
+        text = md(text)
+
+        return text
+
+    if linkedin_url:
+        st.write(f"Fetching data from {linkedin_url}")
+        fetched_data = pull_from_website(linkedin_url)
         
-        if not docs:
-            st.error("Failed to split the fetched data into documents.")
-            return
+        if fetched_data is None:
+            st.write("Failed to fetch data from the website.")
+        else:
+            st.write("Fetched data successfully. Now splitting into documents.")
+            # Split the fetched data into documents
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=20000, chunk_overlap=2000)
+            docs = text_splitter.create_documents([fetched_data])
 
-        map_prompt = """You are a helpful AI bot that aids a user in research.
-        Below is information about a person.
-        Your goal is to generate a brief summary of the person's professional background and expertise.
+            if len(docs) == 0:
+                st.write("Failed to split the fetched data into documents.")
+            else:
+                st.write("Data split into documents successfully. Now generating summary.")
+                # Generate the summary
+                llm = ChatOpenAI(temperature=.25, model_name='gpt-3.5-turbo-16k', api_key=OPENAI_API_KEY)
+                chain = load_summarize_chain(llm)
 
-        % START OF INFORMATION:
-        {text}
-        % END OF INFORMATION:
+                output = chain({"input_documents": docs})
 
-        Please summarize the above information into a brief professional bio.
-
-        YOUR RESPONSE:"""
-        map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
-
-        combine_prompt = """
-        You are a helpful AI bot that aids a user in research.
-        You will be given a list of potential summaries.
-
-        Please consolidate the summaries into a single coherent bio
-
-        % SUMMARIES
-        {text}
-        """
-        combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=["text"])
-
-        llm = ChatOpenAI(temperature=.25, model_name='gpt-3.5-turbo')
-
-        chain = load_summarize_chain(llm,
-                                     chain_type="map_reduce",
-                                     map_prompt=map_prompt_template,
-                                     combine_prompt=combine_prompt_template)
-
-        output = chain({"input_documents": docs})
-        st.write(output['output_text'])
+                st.write("Generated summary successfully.")
+                st.write(output['output_text'])
 
 if __name__ == "__main__":
     main()
