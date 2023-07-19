a10678daa5c05f86afde0ce220ac0f3793b46d62
Marc Hamel <hamelcubsfan@gmail.com>
2023-07-19 17:03:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
-from langchain.chains import StuffDocumentsChain
+from langchain import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter, Document
 from langchain.prompts import PromptTemplate
 import requests
 from bs4 import BeautifulSoup
 from markdownify import markdownify as md
-import re
 
 # Create Streamlit interface
 st.title("Personalized Outreach Generator")
@@ -31,14 +29,12 @@ def pull_from_website(url):
         tag.decompose()
 
     text = soup.get_text()
-    # Remove URLs
-    text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)
     text = md(text)
     return text
 
 map_prompt = """Below is a section of a website about {candidate}
 
-Based only on the text provided, write a concise summary about {candidate}. Do not invent or add any additional information.
+Write a concise summary about {candidate}. If the information is not about {candidate}, exclude it from your summary.
 
 {text}
 
@@ -48,7 +44,7 @@ combine_prompt = """
 You are a helpful AI bot that aids a user in summarizing information.
 You will be given a list of summaries about {candidate}.
 
-Please consolidate the summaries and return a unified, coherent summary, ensuring to only use the information provided and not to fabricate any details.
+Please consolidate the summaries and return a unified, coherent summary
 
 % SUMMARIES
 {text}
@@ -65,22 +61,18 @@ if st.button("Generate"):
 
         # Initialize the necessary classes
         lang_model = ChatOpenAI(openai_api_key=api_key, model_name='gpt-3.5-turbo', temperature=.25)
-        map_llm_chain = LLMChain(llm=lang_model, prompt=map_prompt_template)
-        combine_llm_chain = LLMChain(llm=lang_model, prompt=combine_prompt_template)
 
         # Prepare the data
         documents = RecursiveCharacterTextSplitter().create_documents([scraped_data])
 
         # Initialize and run StuffDocumentsChain
-        summarize_chain = StuffDocumentsChain(llm_chain=map_llm_chain, document_variable_name="text")
+        summarize_chain = load_summarize_chain(lang_model,
+                                               chain_type="map_reduce",
+                                               map_prompt=map_prompt_template,
+                                               combine_prompt=combine_prompt_template)
+        
         summaries = summarize_chain.run({"input_documents": documents, "candidate": candidate_name})
 
-        # Convert summaries to a list of Document objects
-        summaries = [Document(page_content=summary) for summary in summaries]
-
-        summarize_chain = StuffDocumentsChain(llm_chain=combine_llm_chain, document_variable_name="text")
-        consolidated_summary = summarize_chain.run({"input_documents": summaries, "candidate": candidate_name})
-        
-        st.write(consolidated_summary)
+        st.write(summaries)
     else:
         st.write("Please provide all necessary information.")
