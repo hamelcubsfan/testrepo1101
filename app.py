226634c06d606a1072855a9dc72ff16ebc64b53b
Marc Hamel <hamelcubsfan@gmail.com>
2023-07-19 09:04:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,31 @@ from langchain.llms import OpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
 
+# Define your input variables
+input_variables = ["content"]
+
+# Define your prompt templates
+map_prompt = "{content}\nSummarize:"
+combine_prompt = "{summaries}\nGenerate an outreach email based on the above:"
+
+# Initialize the PromptTemplate with the templates and input variables
+map_prompt_template = PromptTemplate(template=map_prompt, input_variables=input_variables)
+combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=input_variables)
+
 # Create Streamlit interface
 st.title("Personalized Outreach Generator")
 
-api_key = st.text_input("Enter your OpenAI API Key")
-source_url = st.text_input("Enter the source URL (LinkedIn profile, tweets, or blog posts)")
+api_key = st.text_input("Enter your OpenAI API Key", key="openai_key")
+source_url = st.text_input("Enter the source URL (LinkedIn profile, tweets, or blog posts)", key="source_url")
 
 if st.button("Generate"):
     if api_key and source_url:
+        # Initialize OpenAI model
         lang_model = OpenAI(openai_api_key=api_key)
 
         # Initialize the necessary classes
         url_loader = UnstructuredURLLoader(urls=[source_url])
         text_splitter = RecursiveCharacterTextSplitter()
-        map_prompt = PromptTemplate("{content}\nSummarize:")
-        combine_prompt = PromptTemplate("{summaries}\nGenerate an outreach email based on the above:")
         summarize_chain = load_summarize_chain()
 
         # Load and prepare the data
@@ -27,7 +37,7 @@ if st.button("Generate"):
         documents = text_splitter.split(documents)
 
         # Generate the personalized outreach email
-        email = summarize_chain.execute(documents, lang_model, map_prompt, combine_prompt)
+        email = summarize_chain.execute(documents, lang_model, map_prompt_template, combine_prompt_template)
 
         st.write(email)
     else:
