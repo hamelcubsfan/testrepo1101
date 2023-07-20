00ba926e207a6824e3911fe5061ed6dee2f84807
Marc Hamel <hamelcubsfan@gmail.com>
2023-07-20 08:49:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,23 +20,31 @@ candidate_name = st.text_input("Enter the candidate's name")
 def pull_from_website(url):
     try:
         response = requests.get(url)
-    except Exception as e:
-        st.write(f"Whoops, error: {e}")
-        return None
+        response.raise_for_status()
+    except requests.HTTPError as http_err:
+        st.write(f"HTTP error occurred: {http_err}")
+        return
+    except Exception as err:
+        st.write(f"Other error occurred: {err}")
+        return
 
-    if response.status_code != 200:
-        st.write(f"Failed to get content from {url}, status code: {response.status_code}")
-        return None
-
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
     # Remove unnecessary tags
     for tag in soup.find_all(['nav', 'footer', 'aside', 'header', 'style', 'script']):
         tag.decompose()
 
-    # Extract only the text within the <p> tags
-    text = ' '.join(p.get_text() for p in soup.find_all('p'))
+    # Scrape only the necessary sections
+    user_info = soup.find('div', class_='user-info')
+    repositories = soup.find('div', class_='repo-list')
 
+    text = ""
+    if user_info is not None:
+        text += user_info.get_text()
+    if repositories is not None:
+        text += repositories.get_text()
+
+    # Convert HTML to Markdown
     h = html2text.HTML2Text()
     text = h.handle(text)
 
@@ -67,26 +75,29 @@ if st.button("Generate"):
     if api_key and source_url and candidate_name:
         # Scrape data from the website
         scraped_data = pull_from_website(source_url)
-        st.write("Scraped Data:", scraped_data)
+        if scraped_data:
+            st.write("Scraped Data:", scraped_data)
 
-        # Initialize the necessary classes
-        lang_model = OpenAI(openai_api_key=api_key, model_name='gpt-3.5-turbo-16k', temperature=.25)
-        map_llm_chain = LLMChain(llm=lang_model, prompt=map_prompt_template)
-        combine_llm_chain = LLMChain(llm=lang_model, prompt=combine_prompt_template)
+            # Initialize the necessary classes
+            lang_model = OpenAI(openai_api_key=api_key, model_name='gpt-3.5-turbo-16k', temperature=.25)
+            map_llm_chain = LLMChain(llm=lang_model, prompt=map_prompt_template)
+            combine_llm_chain = LLMChain(llm=lang_model, prompt=combine_prompt_template)
 
-        # Prepare the data
-        documents = RecursiveCharacterTextSplitter().create_documents([scraped_data])
+            # Prepare the data
+            documents = RecursiveCharacterTextSplitter().create_documents([scraped_data])
 
-        # Initialize and run StuffDocumentsChain
-        summarize_chain = StuffDocumentsChain(llm_chain=map_llm_chain, document_variable_name="text")
-        summaries = summarize_chain.run({"input_documents": documents, "candidate": candidate_name})
+            # Initialize and run StuffDocumentsChain
+            summarize_chain = StuffDocumentsChain(llm_chain=map_llm_chain, document_variable_name="text")
+            summaries = summarize_chain.run({"input_documents": documents, "candidate": candidate_name})
 
-        # Convert summaries to a list of Document objects
-        summaries = [Document(page_content=summary) for summary in summaries]
+            # Convert summaries to a list of Document objects
+            summaries = [Document(page_content=summary) for summary in summaries]
 
-        summarize_chain = StuffDocumentsChain(llm_chain=combine_llm_chain, document_variable_name="text")
-        consolidated_summary = summarize_chain.run({"input_documents": summaries, "candidate": candidate_name})
+            summarize_chain = StuffDocumentsChain(llm_chain=combine_llm_chain, document_variable_name="text")
+            consolidated_summary = summarize_chain.run({"input_documents": summaries, "candidate": candidate_name})
 
-        st.write(consolidated_summary)
+            st.write(consolidated_summary)
+        else:
+            st.write("Failed to scrape the website.")
     else:
         st.write("Please provide all necessary information.")
